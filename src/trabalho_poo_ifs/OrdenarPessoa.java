f5e184d65ce35f8bc46cd2ee15e59eabc5678b44
IgorGoncalves <igor_goncalves@live.com>
2016-02-09 13:48:58 -0300

    Adicionados comentários e editor-fold's

--- src/trabalho_poo_ifs/OrdenarPessoa.java
+++ src/trabalho_poo_ifs/OrdenarPessoa.java
@@ -34,3 +34,4 @@ public enum OrdenarPessoa implements Comparator<Pessoa> {
     }
 
 }
+
--- src/trabalho_poo_ifs/Pessoa.java
+++ src/trabalho_poo_ifs/Pessoa.java
@@ -15,6 +15,7 @@ public class Pessoa implements Comparable<Pessoa> {
     private String nome;
     private String fone;
 
+    // <editor-fold defaultstate="collapsed" desc="Construtores">    
     public Pessoa() {
     }
 
@@ -23,25 +24,9 @@ public class Pessoa implements Comparable<Pessoa> {
         this.nome = nome;
         this.fone = fone;
     }
+    // </editor-fold>
 
-    /**
-     * Get the value of fone
-     *
-     * @return the value of fone
-     */
-    public String getFone() {
-        return fone;
-    }
-
-    /**
-     * Set the value of fone
-     *
-     * @param fone new value of fone
-     */
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
+    // <editor-fold defaultstate="collapsed" desc="Nome"> 
     /**
      * Get the value of nome
      *
@@ -60,10 +45,36 @@ public class Pessoa implements Comparable<Pessoa> {
         this.nome = nome;
     }
 
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Fone"> 
+    /**
+     * Get the value of fone
+     *
+     * @return the value of fone
+     */
+    public String getFone() {
+        return fone;
+    }
+
+    /**
+     * Set the value of fone
+     *
+     * @param fone new value of fone
+     */
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    // </editor-fold>    
+    
+    // <editor-fold defaultstate="collapsed" desc="CPF"> 
     /**
      * Get the value of CPF
      *
-     * @return the value of CPF
+     * @
+     * return the value of CPF
+     *
      */
     public long getCPF() {
         return CPF;
@@ -77,7 +88,9 @@ public class Pessoa implements Comparable<Pessoa> {
     public void setCPF(long CPF) {
         this.CPF = CPF;
     }
+    // </editor-fold>
 
+    // <editor-fold defaultstate="collapsed" desc="Comparação para ordenação"> 
     @Override
     public int compareTo(Pessoa o) {
         if (this.CPF > o.CPF) {
@@ -88,10 +101,13 @@ public class Pessoa implements Comparable<Pessoa> {
         }
         return 0;
     }
+    // </editor-fold>
+
     //Sobrescrita desse metodo permite uma apresentação do objeto na forma de uma string
     @Override
     public String toString() {
         return String.format("CPF: %011d; Nome: %s; Fone: %s; \n", this.CPF, this.nome, this.fone);
     }
 
+    
 }
